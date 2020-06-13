8f598b626a769c0adc2c6db979f65731e4c647e4
s20687-pj <s20687-pj@pjwstk.edu.pl>
2020-06-13 09:43:14 +0200

    completing testing of colors

--- src/test/ColorTest.java
+++ src/test/ColorTest.java
@@ -8,7 +8,7 @@ import java.awt.*;
 import java.util.Arrays;
 import java.util.Random;
 
-import static model.Colour.getBoardColor;
+import static model.Colour.*;
 import static org.junit.Assert.*;
 
 
@@ -20,6 +20,21 @@ public class ColorTest {
         assertEquals(temp, new Color(0xBBADA0));
         Color temp2 = getBoardColor(80);
         assertEquals(temp2, new Color(0xcdc1b4));
+    }
+
+    @Test
+    public void testTileColor() {
+        Color temp = getTileColor(2);
+        assertEquals(temp, new Color(0xeee4da));
+        Color temp2 = getTileColor(80);
+        assertEquals(temp2, new Color(0xcdc1b4));
+    }
+    @Test
+    public void testgetStringColor() {
+        Color temp = getStringColor(8);
+        assertEquals(temp, new Color(0x776e65));
+        Color temp2 = getStringColor(80);
+        assertEquals(temp2, new Color(0xf9f6f2));
     }
 
 }
